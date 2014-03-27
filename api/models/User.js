d3b1963d8971f5ba70aa560aa5142caca8677167
Camilet <camilet@elcamilet.elcamilet.es>
2014-03-27 17:22:25 +0100

    Afegit validación de password

--- api/models/User.js
+++ api/models/User.js
@@ -26,6 +26,14 @@ module.exports = {
 		delete obj.password;
 		return obj;
 	},
+	
+	validPassword: function(password, callback) {
+		var obj = this.toObject();
+		if (callback) {
+			return bcrypt.compare(password, obj.password, callback);
+		}
+		return bcrypt.compareSync(password, obj.password);
+	}
   },
 
   beforeCreate: function(values, next) {
@@ -33,18 +41,39 @@ module.exports = {
   },
 
   beforeUpdate: function(values, next) {
-	if(values.password) hashPassword(values, next);
-	else next();
-  },
+	if(values.password && values.new_password && values.confirm_password) {
+		if (values.new_password === values.confirm_password){
+			User.findOne(values.id).done(function(err, user){
+				if (err) return next(err);
+				if (user.validPassword(values.password)){
+					values.password = values.new_password;
+					delete values.confirm_password;
+					hashPassword(values, next);
+				}
+			});
+		}
+	} else {
+		User.findOne(values.id).done(function(err, user){
+			if (err) {return next(err);}
+			else {
+				values.password = user.password;
+				next();
+			}
+		});
+	}
+  }
 
 };
 
 var bcrypt = require('bcrypt');
 
 function hashPassword(values, next) {
-	bcrypt.hash(values.password, 10, function(err, hash){
-		if(err) return next(err);
-		values.password = hash;
-		next();
+	bcrypt.genSalt(10, function(err, salt){
+		if (err) return next (err);
+		bcrypt.hash(values.password, salt, function(err, hash){
+			if (err) return next(err);
+			values.password = hash;
+			next();
+		});
 	});
 }
