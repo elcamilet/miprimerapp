7213f801e6afcf0eae2b0640f73e55b9e1b48ad9
Camilet <camilet@elcamilet.elcamilet.es>
2014-03-27 16:31:58 +0100

    Atributos de usuario

--- api/models/User.js
+++ api/models/User.js
@@ -9,11 +9,23 @@
 module.exports = {
 
   attributes: {
-  	
-  	/* e.g.
-  	nickname: 'string'
-  	*/
-    
+  	email: {
+		type: 'string',
+		required: true,
+		email: true,
+		unique: true
+	},
+
+	password: {
+		type: 'string',
+		required: true
+	},
+   	 
+	toJSON: function() {
+		var obj = this.toObject();
+		delete obj.password;
+		return obj;
+	}
   }
 
 };
