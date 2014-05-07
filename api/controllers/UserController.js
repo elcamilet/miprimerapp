f88c533bd8367557d0373b22ebcf2216ba45fc14
Camilet <elcamilet@gmail.com>
2014-05-07 13:50:32 +0200

    auth

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -2,7 +2,7 @@
  * UserController
  *
  * @module      :: Controller
- * @description	:: A set of functions called `actions`.
+ * @description :: A set of functions called `actions`.
  *
  *                 Actions contain code telling Sails how to respond to a certain type of request.
  *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
@@ -16,38 +16,161 @@
  */
 
 module.exports = {
-    
+	
+	/*
+	 * API Rest actions.
+	 */
+	find: function(req, res, next) {
+		var id = req.param('id');
+		// If id is a shortcut we don't have to find.
+		if ( isShortcut(id) ) return next();
+
+		// If we get an id we will retun one unique user.
+		if (id) {
+			User.findOne(id).done(function foundUser(err, user){
+				if ( err ) return next(err);
+				if ( !user ) return res.notFound();
+
+				// Response JSON if needed.
+				if (req.wantsJSON) return res.json(user);
+				// Else response view with results 
+				else return res.view({ user: user });
+			});
+		}
+		// Otherwise, we will retun an user array.
+		else {
+			
+			// If we have a where param we will pase it as JSON.
+			var where = req.param('where');
+			if( _.isString(where)) {
+				where = JSON.parse(where);
+			}
+			// Setting options from params.
+			var filters = {
+				limit: req.param('limit') || undefined,
+				skip: req.param('skip')  || undefined,
+				sort: req.param('sort') || undefined,
+				where: where || undefined
+			};
+			// Find users according with filters
+			User.find(filters).done(function foundUsers(err, users){
+				if ( err ) return next(err);
+				// Response JSON if needed.
+				if (req.wantsJSON) {
+					// If there are users return users
+					if( users.length ) return res.json(users);
+					// Otherwise, return status 204: no content
+					else return json(204);
+				// Otherwise, response view with results 
+				} else {
+					return res.view({ users: users });
+				}
+			});
+		}
+		function isShortcut(id){
+			return (id === 'find' || id === 'create' || id === 'update' || id === 'destroy' );
+		}
+	},
+	create: function(req, res, next) {
+		// Create an user using all params.
+		// Schema is true, then we will save that we need.
+		User.create( req.params.all(), function createdUser(err, user){
+			if (err) return next(err);
+			// Response JSON if needed.
+			// Status 201 is Created.
+			if (req.wantsJSON) return res.json(201, user);
+			// Redirect to the user page that we've just created
+			else return res.redirect('/user/' + user.id);
+		});
+	},
+	update: function(req, res, next) {
+		var id = req.param('id');
+		// Id is needed, otherwise not found (status 404).
+		if( !id ) return res.notFound();
+		// Update the user gave.
+		User.update(id, req.params.all()).done(function updatedUsers(err, users){
+			if ( err ) return next(err);
+			// If user is not found, return error 404.
+			if ( !users ) return res.notFound();
+			// Update return an array,
+			// but as we update by id, we can take the unique element.
+			var user = users[0];
+			// Response JSON if needed.
+			// Status 201 is Created.
+			if (req.wantsJSON) return res.json(201, user);
+			// Redirect to the user page that we've just created.
+			else return res.redirect('/user/' + user.id);
+		});
+	},
+	destroy: function(req, res, next) {
+		var id = req.param('id');
+		// Id is needed, otherwise not found (status 404).
+		if( !id ) return res.notFound();
+		// Find the user by id.
+		User.findOne(id).done(function foundUser(err, user){
+			if ( err ) return next(err);
+			// If user is not found, return error 404.
+			if ( !user ) return res.notFound();
+			// Delete the user.
+			User.destroy(user.id).done(function userDestroyed(err){
+				if ( err ) return next();
+				// Response JSON if needed.
+				if (req.wantsJSON) return res.json(200);
+				// Redirect to the users page.
+				else return res.redirect('/user');
+			})
+		});
+	},
+	
+	/*
+	 * Actions to render a view.
+	 */
+	new: function(req, res) {
+		// Response the view with the action's name.
+		return res.view();
+	},
+	edit: function(req, res) {
+		User.findOne(req.param('id')).done(function foundUser(err, user){
+			if ( err ) return next(err);
+			// Response the view with the action's name.
+			else return res.view({ user: user });
+		});
+	},
+	// This accion will render the view with the login form
 	auth: function(req, res) {
 		return res.view();
-	}
-
+	},
+	
+	/*
+	 * Actions that proccess info.
+	 */
 	login: function(req, res) {
-		User.findOne({email: req.param('email')}).done(function foundUser(err, user){
-			if ( err || !user ) return res.redirect('/auth');
+		// Get the unique user with this email.
+		User.findOne({email: req.param('email')}).done(function(err, user){
+			// If there are an error,
+			// or the user doesn't exist,
+			// return to auth page.
+			// TODO: Error handler.
+			if ( err || !user ) return res.redirect('/user/auth');
 			require('bcrypt').compare(req.param('password'), user.password, function(err, valid){
-				if(err || !valid ) return res.redirect('/auth');
+				// If there are an error,
+				// or the pass doesn't match,
+				// return to auth page.
+				// TODO: Error handler.
+				if(err || !valid ) return res.redirect('/user/auth');
+				// Set autenticated to true.
 				req.session.authenticated = true;
+				// save the user data in the session.
 				req.session.user = user;
+				// Redirect to the user page.
 				return res.redirect('/user/' + user.id);
 			});
 		});
-	}
-
-	create: function(req, res, next) {
-		User.create( req.params.all(), function createdUser(err, user){
-			[…]
-			req.session.authenticated = true;
-			req.session.user = user;
-			[…]
-		});
-	}
-  
+	},
 	logout: function(req, res){
+		// Destroy the session.
 		req.session.destroy();
+		// Redirect to home page.
 		return res.redirect('/');
 	}
-
-	_config: {}
-
-  
 };
